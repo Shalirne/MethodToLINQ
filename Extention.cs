bc501657b22b836960add1315727168e92d4aa50
1 <1@COMPUTER>
2023-09-26 21:07:29 +0500

    Добавьте файлы проекта.

---
+++ Extention.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodToLINQ
+{
+    public static class Extention
+    {
+        public static IEnumerable Top<T>(this IEnumerable<T> InputCollection, int Sample)
+        {
+
+            if ((InputCollection.Count() > 100) | (InputCollection.Count() < 1))
+            {
+                throw new ArgumentException("Передана некорректная информация");
+            }
+            float fraction = (float)InputCollection.Count() * Sample / 100;
+            var number = (int)fraction;
+            if (number != fraction)
+            {
+                number += 1;
+            }
+            IEnumerable outputCollection = (from a in InputCollection
+                                            orderby a descending
+                                            select a).Take(number).ToList();
+            return outputCollection;
+        }
+
+        public static IEnumerable Top<T>(this IEnumerable<T> InputCollection, int Sample, Func<Person, int> Age)
+        {
+            float fraction = (float)InputCollection.Count() * Sample / 100;
+            var number = (int)fraction;
+            if (number != fraction)
+            {
+                number += 1;
+            }
+            List<int> primarySelection = new List<int>();
+            Program program = new Program();
+            foreach (var a in Program.person)
+            {
+                primarySelection.Add(Age(a));
+            }
+            IEnumerable outputCollection = (from a in primarySelection
+                                            orderby a descending
+                                            select a).Take(number).ToList();
+            return outputCollection;
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace MethodToLINQ
+{
+    internal class Program
+    {
+        public static List<Person> person = new List<Person>();
+        static void Main(string[] args)
+        {
+            String[] name = { "Игорь", "Николай", "Руслан", "Алексей", "Юрий", "Ярослав", "Семен", "Евгений", "Олег", "Артур", "Петр", "Степан", "Илья" };
+            var rnd = new Random();
+            List<int> IEnumerable1 = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                IEnumerable1.Add(rnd.Next(0, 10000));
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                person.Add(new Person { Name = name[i], Age = rnd.Next(20, 55) });
+            }
+            var selectedNumbers = IEnumerable1.Top(41);
+            var selectedUsers = person.Top(33, p => p.Age);
+            Console.WriteLine(JsonSerializer.Serialize(selectedNumbers));
+            Console.WriteLine(JsonSerializer.Serialize(selectedUsers));
+        }
+    }
+}
